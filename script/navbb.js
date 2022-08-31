65bc292fbb34cde66c83c1bc0256094d41e3b1d9
Juan Pablo <juampi_reviglio@hotmail.com>
2022-08-31 09:08:36 -0300

    prueba proyecto RGL

---
+++ script/navbb.js
@@ -0,0 +1,11 @@
+var  nav = document.getElementById('navbb');
+    window.onscroll = function(){
+
+    if (window.pageYOffset >100) {
+
+    nav.style.background = "#C2BDB8";
+    }
+    else{
+    nav.style.background = "transparent";
+    }
+}
---
+++ script/script.js
@@ -0,0 +1,11 @@
+var  nav = document.getElementById('navb');
+    window.onscroll = function(){
+
+    if (window.pageYOffset >100) {
+
+    nav.style.background = "#3A3737";
+    }
+    else{
+    nav.style.background = "transparent";
+    }
+}
