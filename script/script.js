374d1d390914ea56b214403fdc7aace17027a323
Juan Pablo <juampi_reviglio@hotmail.com>
2022-10-20 01:09:02 -0300

    responsive

--- script/script.js
+++ script/script.js
@@ -9,15 +9,3 @@ var  nav = document.getElementById('navb');
     nav.style.background = "transparent";
     }
 }
-
-const checkpoint = 300;
- 
-window.addEventListener("scroll", () => {
-  const currentScroll = window.pageYOffset;
-  if (currentScroll <= checkpoint) {
-    opacity = 1 - currentScroll / checkpoint;
-  } else {
-    opacity = 0;
-  }
-  document.querySelector(".contenedor-gif-logo-rgl").style.opacity = opacity;
-});
